15c9f57b3f8bddf0b4e8c7bb644c0efc7bfff0b8
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-10 23:30:08 -0800

    commit tree 42ea9fdd130536f429807bdc626a096fbaf804ad

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+let message;
+function scuberGreetingForFeet(feet){
   // Write your code here!
+
+  if (feet <= 400) {
+    message = "This one is on me!"
+  }
+  else if (feet > 2000 && feet < 2500) {
+    message = "I will gladly take your thirty bucks."
+  }
+  else if (feet > 2500) {
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city === 'NYC' ? (message = 'Ok, sounds good.') : (message = "No go.");
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      message = "Thank you so much.";
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+      break;
+  }
+  return message;
 }
